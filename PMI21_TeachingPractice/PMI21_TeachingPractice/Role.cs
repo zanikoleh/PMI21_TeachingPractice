089c485fe80308e64f5704c5a77d3c7d2ce0ffb1
likerrr777 <likerrr777@gmail.com>
2014-03-26 15:10:30 +0200

    Added class Role

---
+++ PMI21_TeachingPractice/PMI21_TeachingPractice/Role.cs
@@ -0,0 +1,30 @@
+﻿namespace UsersProj
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// class Role represents a role and id of a user
+    /// </summary>
+    class Role
+    {
+        public int Id { get; private set; }
+        public string Duty { get; private set; }
+
+        public Role()
+        {
+            this.Id = -1;
+            this.Duty = "norole";
+        }
+
+        public Role(int id, string duty)
+        {
+            this.Id = id;
+            this.Duty = duty;
+        }
+    }
+}
+
