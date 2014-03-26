1b03a546941e1ec056032374b0759ee42281a755
likerrr777 <likerrr777@gmail.com>
2014-03-26 15:11:07 +0200

    Added class Constants

---
+++ PMI21_TeachingPractice/PMI21_TeachingPractice/Constants.cs
@@ -0,0 +1,20 @@
+﻿namespace UsersProj
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// This class is created in order to avoid using magic numbers in the program code
+    /// </summary>
+    class Constants
+    {
+        public const int Zero = 0;
+        public const int One = 1;
+        public const int Two = 2;
+        public const int Three = 3;
+
+    }
+}
