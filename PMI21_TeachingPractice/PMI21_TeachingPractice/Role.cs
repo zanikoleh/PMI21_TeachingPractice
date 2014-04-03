5dcf66293d6a180683ea2a84cab5e2f828f8d34b
likerrr777 <likerrr777@gmail.com>
2014-04-03 12:07:15 +0300

    Renamed RoleAndId to Role

---
+++ PMI21_TeachingPractice/PMI21_TeachingPractice/Role.cs
@@ -0,0 +1,30 @@
+﻿namespace UsersProj
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// class Role represents a role and id of a user
+    /// </summary>
+    public class Role
+    {
+        public Role()
+        {
+            this.Id = -1;
+            this.Name = "Default";
+        }
+
+        public Role(int id, Constants.Roles duty)
+        {
+            this.Id = id;
+            this.Duty = duty;
+        }
+
+        public int Id { get; private set; }
+
+        public string Name { get; private set; }
+    }
+}
