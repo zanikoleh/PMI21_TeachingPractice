02b8ec47f84bee7daa81014c4d53ae030a97b451
Strenkomendencha <strenkomendencha.gmail.com>
2014-04-18 13:40:30 +0300

    Password masking in console added.

--- PMI21_TeachingPractice/PMI21_TeachingPractice/Application.cs
+++ PMI21_TeachingPractice/PMI21_TeachingPractice/Application.cs
@@ -15,7 +15,31 @@ namespace PMI21_TeachingPractice
                 Console.WriteLine("login:");
                 string login = Console.ReadLine();
                 Console.WriteLine("password:");
-                string password = Console.ReadLine();
+                ConsoleKeyInfo key;
+                string password = string.Empty;
+                do
+                {
+                    key = Console.ReadKey(true);
+
+                    // Backspace Should Not Work
+                    if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+                    {
+                        password += key.KeyChar;
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        if (key.Key == ConsoleKey.Backspace && password.Length > 0)
+                        {
+                            password = password.Substring(0, (password.Length - 1));
+                            Console.Write("\b \b");
+                        }
+                    }
+                }
+                // Stops Receving Keys Once Enter is Pressed
+                while (key.Key != ConsoleKey.Enter);
+
+                Console.WriteLine();
                 if (uc.Identify(login, password) == null)
                 {
                     if (count < 3)
